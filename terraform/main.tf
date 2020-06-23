cef0be97837b767de680e58a608f0b1710dc583b
Peter Latham <pl@udbudsvagten.dk>
2020-06-23 14:30:46 +0200

    Add some terraform to the workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,10 +10,6 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.region
-
-  assume_role {
-    role_arn = var.assume_role_arn
-  }
 }
 
 data "aws_availability_zones" "available" {}
