3064f6d40e7ad28fa84f6a83f20db98fdc9bafb1
Peter Latham <pl@udbudsvagten.dk>
2020-06-23 22:39:09 +0200

    changing tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ locals {
   common_tags = {
     Name        = "${var.environment}-sd-frontend"
     Environment = var.environment
-    Test        = "brand new tag"
+    Test        = "revert old tag"
   }
 }
 
