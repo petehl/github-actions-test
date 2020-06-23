d29d8130f7369097e0dd31ade45f699330da4cec
Peter Latham <pl@udbudsvagten.dk>
2020-06-23 21:40:59 +0200

    update tags to test deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ locals {
   common_tags = {
     Name        = "${var.environment}-sd-frontend"
     Environment = var.environment
-    Test        = "update-tags-only-me"
+    Test        = "brand new tag"
   }
 }
 
